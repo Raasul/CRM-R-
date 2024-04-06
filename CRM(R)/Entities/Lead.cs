d151d45af5db0150ef0e7320310c4546a41690c4
Raasul <rasul.toktorbekov@gmail.com>
2024-04-06 16:09:28 +0600

    Add project files.

---
+++ CRM(R)/Entities/Lead.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRM_R_.Entities
+{
+    public class Lead
+    {
+        public string Id { get; set; }
+        [Required]
+        public int EntityContact {  get; set; }
+        public int? EntityUserId { get; set; }
+        [Required]
+        public LeadStatus Status { get; set; }
+        [Required]
+        public EntityConatct? Contact {  get; set; }
+        public EntityUser? User { get; set; }
+        public List<>
+    }
+}
---
+++ CRM(R)/Entities/Sale.cs
@@ -0,0 +1,11 @@
+﻿namespace CRM_R_.Entities
+{
+    public class Sale
+    {
+        public int Id { get; set; }
+        [Required]
+        public int EntityLeadId { get; set; }
+        [Required]
+
+    }
+}
